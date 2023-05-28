5b5f9a30a6d9e0f71aadcae46380cc764bc37ec2
Er1ck-Esp1n0sa <zs20006728@estudiantes.uv.mx>
2023-05-27 19:19:40 -0600

    Update13

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -50,17 +50,17 @@ for msg in consumer:
        print("Could not insert into MongoDB")
 
     try:
-        agg_result = db.memes_info.aggregate(
+        agg_result = db.socer_info.aggregate(
             [{
                 "$group" : 
                 { "_id" : "$name",
                   "n" : {"$sum":1}}
             }]
         )
-        db.memes_summary.delete_many({})
+        db.socer_summary.delete_many({})
         for i in agg_result:
             print(i)
-            summary_id = db.memes_summary.insert_one(i)
+            summary_id = db.socer_summary.insert_one(i)
             print("Summary inserted with record ids", summary_id)
        #meme_rec = {'name':name }
        #print (meme_rec)
